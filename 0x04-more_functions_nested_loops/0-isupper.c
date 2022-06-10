ad2bc6d4c9bfd859bde6ae98875d1fbb2a669d24
biche1 <bichechi101@gmail.com>
2022-06-10 05:23:53 -0700

    task0

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -10,9 +10,13 @@ int main(void)
 {
 	char c;
 
-	c = 'A';
-	printf("%c: %d\n", c, _isupper(c));
-	c = 'a';
-	printf("%c: %d\n", c, _isupper(c));
+	if (c == A)
+	{
+		printf("%c: %d\n", c, _isupper(c));
+	}
+	else (c = a)
+	{
+		printf("%c: %d\n", c, _isupper(c));
+	}
 	return (0);
 }
