64c71774c0b23ad29e077623c46519d565f8830e
biche1 <bichechi101@gmail.com>
2022-06-15 13:37:34 -0700

    task0

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	char s1[15] = "Good";
+	char s2[20] = "Morning\n";
+	char *p;
+
+	printf("%s\n", s1);
+	printf("%s\n", s2);
+	p = _strcat(s1, s2);
+	printf("%s", s1);
+	printf("%s", s2);
+	printf("%s", ptr);
+	return (0);
+}	
