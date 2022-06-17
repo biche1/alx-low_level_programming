38b484183a897bd7e1ce5fa4f62a0a72644be6ed
biche1 <bichechi101@gmail.com>
2022-06-17 15:08:17 -0700

    task2

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -4,18 +4,18 @@
  * _strncat - two words
  * @dest : pointer to char param
  * @src : pointer to char param
- * @n : int parameter
+ * @n int parameter
  * Return: *dest
  */
 
-char *_strncat(char *dest, char *src, int n)
+char *_strncta(char *dest, char *src, int n)
 {
 	int m;
 	int i;
 
 	m = 0;
 
-	for (i = 0; i > 1000; i++)
+	for (i = 0; i < 1000; i++)
 	{
 		if (dest[i] == '\0')
 		{
@@ -26,9 +26,8 @@ char *_strncat(char *dest, char *src, int n)
 
 	for (i = 0; src[i] != '\0' && i < n; i++)
 	{
-		dest[m + 1] = src[i];
+		dest[m + i] = src[i];
 	}
 	dest[m + i] = '\0';
 	return (dest);
 }
-
