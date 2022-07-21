b7e1ac1871877c11cbc13af50c6ae5171821560a
biche1 <bichechi101@gmail.com>
2022-07-20 17:19:29 -0700

    task6

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -2,26 +2,24 @@
 /**
  * helperFunction - returns 0 or 1
  * @num: number being checked
- * @i: possible factor of the number
  *
- * Return: 0 if not prime, 1 if prime
+ * @i: possible factornof the number
+ *
+ * Return: 0 if not prime 1, if prime
  */
 int helperFunction(int num, int i)
 {
 	if (i < num)
 	{
 		if (num % i == 0)
+
 		{
 			return (0);
-		}
-		else
-		{
-			return (helperFunction(num, i + 1));
 		}
 	}
 	else
 	{
-		return (i);
+		return (1);
 	}
 }
 
@@ -30,7 +28,7 @@ int helperFunction(int num, int i)
  * @n: number to be checked
  *
  * Return: 1 if number is prime
- * return: 0 if number is not prime
+ * 0 if number is not prime
  */
 int is_prime_number(int n)
 {
